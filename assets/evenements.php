51921e8052edbb9d879d0f97fc392f6cbd5a0dbf
VENUTI MASSIMO <massimo.venuti@etu.unistra.fr>
2020-02-20 16:14:33 +0100

    Commit de fin de journée - 20/02/2020

--- assets/evenements.php
+++ assets/evenements.php
@@ -13,10 +13,6 @@
 </head>
 <body>
     <?php include("templates/navbar.php"); ?>
-    <header>
-        <h1>Association des Étudiants Étrangers</h1>
-        <h2>AEE</h2>
-    </header>
 
 
 
@@ -26,6 +22,7 @@
 
 
     <?php include("templates/footer.php"); ?>
+    
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
---
+++ templates/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <a class="navbar-brand" href="#accueil">AEE</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#AEE">QUI SOMMES NOUS ?</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#evenements">ÉVÈNEMENTS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#actus">ACTUS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#galerie">GALERIE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#contact">CONTACT</a>
+            </li>
+        </ul>
+    </div>
+</nav>
